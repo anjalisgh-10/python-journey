a303033c32423e8517f2a0dc59099d27b546cab9
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-21 01:44:37 +0530

    Input from user

---
+++ 01.Python/02.Print statement/input_from_user.py
@@ -0,0 +1,12 @@
+num1 = input("Enter number 1 = ")
+num2 = input("Enter number 2 = ")
+
+total = num1 + num2
+print(f"Total = {total}")
+
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+total = num1 + num2
+print(f"Total = {total}")
