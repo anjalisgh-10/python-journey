d544456cd295303aaf45d75204d8dca1c9107efc
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-22 17:18:55 +0530

    Escape sequence and operator

---
+++ 01.Python/03.Escape sequence and operators/escape_sequence.py
@@ -0,0 +1,22 @@
+""" 
+\n - New Line
+\t - Tab Space
+\\ - Backslash
+\" - Double quotes
+\' - Single quotes
+"""
+
+print("Hello World")
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+# Anjali\Singh
+print("Anjali\\Singh")
+
+# Anjali\\\Singh
+print("Anjali\\\\\\Singh")
+
+# My name is "Anjali" Singh
+print("My name is 'Anjali' Singh")
+print('My name is "Anjali" Singh')
+print("My name is \"Anjali\"Singh")
