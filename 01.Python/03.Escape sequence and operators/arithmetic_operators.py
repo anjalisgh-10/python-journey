56be2ee82371a4a546c330a4358234d6c64f0cec
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-22 17:49:32 +0530

    arithmetic operators

---
+++ 01.Python/03.Escape sequence and operators/arithmetic_operators.py
@@ -0,0 +1,17 @@
+a = 29
+b = 10
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Floor division (int)
+print(a // b)
+
+#  Exponent
+print(2**5)   # Base =2, exp =5 -> 2x2x2x2x2
+print(100**0.5)   # Base =100, exp =0.5
+
+# Modulus (rwmainder)
+print(19 % 5)
+print(24 % 10)
