d96e14e0d6b2e732f12159a36dfda6ea822f36df
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-22 18:46:04 +0530

    logical operators

---
+++ 01.Python/03.Escape sequence and operators/logical_operators.py
@@ -0,0 +1,23 @@
+# AND, OR, NOT
+
+chemistry = 45
+physics = 35
+
+# print True if pass in both subjects
+print(chemistry > 33 and physics > 33)
+
+# print True if pass in any subjects
+print(chemistry > 33 or physics > 33)
+
+chemistry = 15
+physics = 18
+
+
+# print True if pass in both subjects
+print(chemistry > 33 and physics > 33)
+
+# print True if pass in any subjects
+print(chemistry > 33 or physics > 33)
+
+# print(not chemitry > 33 and physics > 33)
+print(not (chemistry > 33 and physics > 33))
