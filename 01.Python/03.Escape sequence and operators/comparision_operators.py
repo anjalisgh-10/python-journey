6aaa1e67587a125fe05d2013525a5488e19d13f9
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-22 18:09:48 +0530

    comparision operators

---
+++ 01.Python/03.Escape sequence and operators/comparision_operators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a > b)
+print(a < b)
+
+a = 10
+b = 10
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a > b)
+print(a < b)
