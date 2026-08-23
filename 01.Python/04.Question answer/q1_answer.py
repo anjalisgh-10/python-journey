dfbac3b429603862b24240ddfc8e10357828f46f
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-23 21:35:40 +0530

    question answer

---
+++ 01.Python/04.Question answer/q1_answer.py
@@ -0,0 +1,11 @@
+"""
+Q1: Take two number as input from the uder. Print their sum, difference, product, and remainder.
+
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+print(f"Sum = {num1+num2}")
+print(f"Difference = {num1-num2}")
+print(f"Product = {num1*num2}")
+print(f"Remainder = {num1%num2}")
