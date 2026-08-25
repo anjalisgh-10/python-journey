31a22bfc88a8e41a4a752ca9c2af9d9e482f88d9
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-25 22:40:30 +0530

    question answer

---
+++ 01.Python/04.Question answer/q6_answer.py
@@ -0,0 +1,15 @@
+"""
+Take two number as input. Print the greatest of the two.
+If they are equal, print "Both are equal."
+
+"""
+
+num1 = int(input("Enter num1 = "))
+num2 = int(input("Enter num2 = "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num2 > num1:
+    print(f"{num2} is greater than {num1}")
+else:
+    print("Both are equal")
