79283ca3baa4ffaf59fb1a9982338295143d283d
Anjali Singh <cs.anjalisgh@gmail.com>
2026-08-30 19:18:52 +0530

    question answer

--- 01.Python/04.Question answer/q17_answer.py
+++ 01.Python/04.Question answer/q17_answer.py
@@ -5,7 +5,13 @@
 4 3 2 1
 5 4 3 2 1
 """
-for i in range(1, 6):
+# for i in range(1, 6):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+n = int(input("Enter a number/lines = "))
+for i in range(1, n + 1):
     for j in range(i, 0, -1):
         print(j, end=" ")
     print()
