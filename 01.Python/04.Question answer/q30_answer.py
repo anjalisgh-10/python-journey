bdbc71f08530e0fb72d52323b213ba51dbd37ba3
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-02 22:45:43 +0530

    que ans

---
+++ 01.Python/04.Question answer/q30_answer.py
@@ -0,0 +1,11 @@
+"""
+write a function calles rectangle_area that takes length and bredth
+as a parameters and prints the area.
+"""
+
+def rectangle_area(length, breadth):
+    area = length * breadth
+    print(f"Area of a rectangle is {area}")
+
+rectangle_area(10, 20)
+rectangle_area(4, 2)
