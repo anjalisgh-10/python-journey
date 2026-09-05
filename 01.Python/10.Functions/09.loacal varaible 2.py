6c4c9b4f80326b5eb863032455cdcf5d213fc62e
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-05 12:32:48 +0530

    local

---
+++ 01.Python/10.Functions/09.loacal varaible 2.py
@@ -0,0 +1,17 @@
+def xyz(n1, n2):
+    n1 = 100
+    n2 = 200
+    print(f"Insde function n1={n1} and n2={n2}")
+
+# a = 10
+# b = 20
+# xyz(10, 20)
+# print(a)
+# print(b)
+
+
+n1 = 10
+n2 = 20
+xyz(n1, n2)
+print(n1)
+print(n2)
