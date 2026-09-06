f5dab35d4f7efbcb1549bbccb3d1e6ae6b574315
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-06 11:35:25 +0530

    que ans

---
+++ 01.Python/04.Question answer/q36_answer.py
@@ -0,0 +1,11 @@
+"""
+Create a list of 5 numbers. Replace the second and fourth elements of this with
+the number 0 using indexing.
+Print the update list
+
+"""
+
+lst = [10, 20, 30, 40, 50]
+lst[1] = 0
+lst[3] = 0
+print(lst)
