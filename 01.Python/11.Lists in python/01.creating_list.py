4df7d60058453ff0d6437f2a2493c416d438c5b1
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-06 10:52:24 +0530

    creating list

--- 01.Python/11.Lists in python/01.creating_list.py
+++ 01.Python/11.Lists in python/01.creating_list.py
@@ -1,4 +1,13 @@
 # Creating a list
-names = ["Anjali", "Rahul", "Nihar", 55, 48.678, True, False]
-print(names)
-print(type(names))
+# names = ["Anjali", "Rahul", "Nihar", 55, 48.678, True, False]
+# print(names)
+# print(type(names))
+
+marks = [3, 7, 88, 78, 19]
+marks2 = [1, 1, 2, 54, 18]
+
+# List operation - +,-,/,*
+# print(marks * 4)
+# print(marks / marks2)
+
+print(marks + marks2)
