64aba203c73d86f3dfe9fd219e5eb661f40d431c
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-08 20:10:23 +0530

    que ans

---
+++ 01.Python/04.Question answer/q38_answer.py
@@ -0,0 +1,17 @@
+"""
+Write a program that takes a list and a target numbers. Use a loop
+to determine the target number exists in the list. Do not use the in operator.
+"""
+
+def does_target_exists(lst, target):
+    for num in lst:
+        if num == target:
+            return True
+    return False
+        
+nums = [6, -5, 4, 2, 10, 91, -75, 49, 9]
+
+print(does_target_exists(nums, 18))
+print(does_target_exists(nums, 9))
+print(does_target_exists(nums, 10))
+print(does_target_exists(nums, 91))
