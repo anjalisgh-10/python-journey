54f72d4dee331b3aacb3a63351f26352084d06d5
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-09 23:11:15 +0530

    append insert remove and pop method

---
+++ 01.Python/11.Lists in python/09.append, insert, remove and pop methods.py
@@ -0,0 +1,13 @@
+lst = ["Anjali", 54, 21.98, True, "Surat"]
+
+print(lst, id(lst))
+# lst.append(100)
+# lst.append("Delhi")
+# lst.insert(2, "Delhi")
+# lst.insert(0, "Muskan")
+
+# lst.pop()
+# lst.pop(0) # By index
+
+lst.remove(54)
+print(lst, id(lst))
