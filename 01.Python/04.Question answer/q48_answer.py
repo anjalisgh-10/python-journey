e39eadc4be0cc6839cd9180c68145a200bc66374
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-12 22:00:26 +0530

    que ans

---
+++ 01.Python/04.Question answer/q48_answer.py
@@ -0,0 +1,13 @@
+"""
+Write a python script that iterates through a list of integer and replaces every
+negative number found in the list with the value 0.
+"""
+def replace_negative(lst):
+    n = len(lst)
+    for i in range(0, n):
+        if lst[i] < 0:
+            lst[i] = 0
+    return  lst
+
+nums = [1, 4, -1, -45, -100, 6, 7, 8, -1]
+print(replace_negative(nums))
