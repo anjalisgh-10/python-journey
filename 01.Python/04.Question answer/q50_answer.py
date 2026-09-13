92b010cb51dc6a480ecb2ad9e4a6fc94085b787e
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-13 21:24:54 +0530

    que ans

---
+++ 01.Python/04.Question answer/q50_answer.py
@@ -0,0 +1,8 @@
+"""
+Given a list of marks, use list comprehension to create a new list that contains
+only tthe marks that aree above 75.
+"""
+marks = [45, 87, 94, 37, 89, 90, 99, 32, 11]
+new_list = [num for num in marks if num >= 75]
+print(marks, id(marks))
+print(new_list, id(new_list))
