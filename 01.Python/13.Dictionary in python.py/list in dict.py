bdadf2e6f72d33c3f31bfac92720a5165b0edd4e
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-21 21:05:58 +0530

    list in dict

---
+++ 01.Python/13.Dictionary in python.py/list in dict.py
@@ -0,0 +1,14 @@
+student = {
+    "name": "Rahul",
+    "age": 21,
+    "subject": ["Maths", "Science", "English"],
+    "marks": [85, 92, 78],
+
+}
+
+print(student["name"])
+print(student["subject"][0])
+print(student["marks"][0])
+print(student["subject"][2])
+
+print(sum(student["marks"]))
