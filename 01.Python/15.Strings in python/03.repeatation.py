89ecb7abd88bbf1f38f14c573e74b47c40fbcbe9
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-25 21:51:22 +0530

    repeatation

---
+++ 01.Python/15.Strings in python/03.repeatation.py
@@ -0,0 +1,15 @@
+first = "Rahul"
+last = "Sharma"
+
+print(first + last) #contact
+print(first + " " + last + "5")
+
+
+print(first * 10)
+x = "5"
+print(x * 10)
+
+print(first - last)
+print(first * last)
+print(first / last)
+print(first / 5)
