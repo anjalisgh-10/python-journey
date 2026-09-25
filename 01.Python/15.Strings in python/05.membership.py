7beb3503f0ffe0a6cf6659a9106661ddf869d185
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-25 22:21:15 +0530

    membership

--- 01.Python/15.Strings in python/05.membership.py
+++ 01.Python/15.Strings in python/05.membership.py
@@ -3,8 +3,14 @@ text = "Programming"
 # in \ not in
 
 
-print("p" in text)
-print("P" in text)
-print("z" not in text)
-print("m" not in text)
-print("gram" not in text)
+# print("p" in text)
+# print("P" in text)
+# print("z" not in text)
+# print("m" not in text)
+# print("gram" not in text)
+
+total = 0
+for ch in text:
+    if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u":
+        total += 1
+print(total)   
