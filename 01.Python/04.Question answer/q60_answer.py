2c780f29620716734232a78bbb719dafe2207752
Anjali Singh <cs.anjalisgh@gmail.com>
2026-09-26 13:06:40 +0530

    que ans

---
+++ 01.Python/04.Question answer/q60_answer.py
@@ -0,0 +1,11 @@
+"""
+Reverse a string
+
+Accept a string as input. Print its reverse using string slicing.
+"""
+
+def reverse_a_string(text):
+    return text[::-1]
+
+ans = reverse_a_string("Anjali")
+print(ans)
